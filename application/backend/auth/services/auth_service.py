c64f712e7f407cc18d99afa5a88fada0c7cc5c07
Aketzali Amparo Zeledon <azeledon@mail.sfsu.edu>
2025-11-30 06:22:03 -0800

    only push auth files rest of files not uptodate(remember to add register router to main.py for testing): hashing added, matching registration, tested with frontend and on http://127.0.0.1:8000/docs#/ for endpoints.

--- application/backend/auth/services/auth_service.py
+++ application/backend/auth/services/auth_service.py
@@ -1,15 +1,29 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from search.models.user import User
+from passlib.context import CryptContext
 import hashlib
- #need to change to email
+'''
+Known User 
+email: tim.jim@sfsu.edu
+pw: test12
+'''
+pwd_context = CryptContext(schemes =["argon2"], deprecated="auto")
+
 def get_user(db: Session, email:str):
-    return db.query(User).filter(User.sfsu_email == email).first()
+    return db.query(User).filter(func.lower(User.sfsu_email) == func.lower(email)).first()
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.user_id == user_id).first()
 
 def authenticate_user(db: Session, email:str, password:str):
     user = get_user(db, email)
     if not user:
+        print("no user found for email:", email)
+        return None
+    print("hash in DB: ", user.password_hash)
+    if not pwd_context.verify(password, user.password_hash):
+        print("pwd hash verification failed")
         return None
     
-    if user.password_hash != password:
-        return None
     return user
