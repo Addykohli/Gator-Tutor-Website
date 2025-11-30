ad310101e97d7f7795bd94a016c512afadc13565
Aketzali Amparo Zeledon <azeledon@mail.sfsu.edu>
2025-11-30 01:09:29 -0800

    added finding all chats one user is in, added endpoints for chatroomhistory between 2 users, added allchats endpoint for all chats one user is apart of

---
+++ application/backend/chat/routers/chat_router.py
@@ -0,0 +1,34 @@
+from chat.schemas.chat_schemas import MessageInfo, MessageResponse
+from chat.services.chat_service import send_message, get_chat, get_user_chats
+from auth.services.auth_service import get_user_by_id
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+
+
+from search.database import get_db
+
+router = APIRouter(prefix="/api/chat", tags=["chat"])
+#send endpoint
+@router.post("/send", response_model=MessageResponse)
+def send_endpoint(req:MessageInfo, db: Session = Depends(get_db), user_id:int = None):
+    if user_id is None:
+        raise HTTPException(status_code =400, detail =" user_is not found")
+
+    sender = get_user_by_id(db, user_id)
+    if not sender:
+        raise HTTPException(status_code = 404, detail="sender id not found")
+    
+    message = send_message(db, user_id, req)
+    return  message
+
+#chat endpoints for all chat messages between current 2 users
+@router.get("/chatroomhistory/{user1}/{user2}", response_model = list[MessageResponse])
+def get_chat_endpoint(user1:int, user2:int, db: Session = Depends(get_db)):
+    messages = get_chat(db, user1, user2)
+    return messages
+
+#get all chats one user is in(ie all messages associated with user)
+@router.get("/allchats/{user_id}", response_model=list[int])
+def get_user_chats_endpoint(user_id:int, db: Session = Depends(get_db)):
+    related_chats = get_user_chats(db, user_id)
+    return related_chats
--- application/backend/chat/services/chat_service.py
+++ application/backend/chat/services/chat_service.py
@@ -12,7 +12,7 @@ def send_message(db: Session, sender_id: int, data):
     db.refresh(chat_message)
     return chat_message
 #convos determined by the 2 users conntected to messages, meaning 2 users cant have diff chat
-def get_conversation(db:Session, user1:int, user2:int):
+def get_chat(db:Session, user1:int, user2:int):
     return(
         db.query(ChatMessage)
         .filter(
@@ -20,3 +20,19 @@ def get_conversation(db:Session, user1:int, user2:int):
             ( (ChatMessage.sender_id == user2)&(ChatMessage.reciever_id ==user1) )
         ).order_by(ChatMessage.created_at.asc()).all()
     )
+#find all chats that individual user is involved in
+def get_user_chats(db:Session, user_id:int):
+    related_messages=(
+        db.query(ChatMessage).filter( 
+            (ChatMessage.sender_id ==user_id) | (ChatMessage.reciever_id == user_id) 
+        ).all()
+    )
+    #other users to differentiate the chats
+    other_users = set()
+    for message in related_messages:
+        if message.sender_id == user_id:
+            other_users.add(message.reciever_id)
+        else:
+            other_users.add(message.sender_id)
+
+    return list(other_users)
